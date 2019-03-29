5ab964547f51387d26abcbf288a4c76f2c7dc57c
Mohamed Saleh <msaleh.eg87@gmail.com>
2019-03-29 09:38:12 +0200

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { useTransition, animated } from 'react-spring'
+import data from './data'
+import shuffle from 'lodash/shuffle'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function App() {
+  const [rows, set] = useState(data)
+  useEffect(() => void setInterval(() => set(shuffle), 2000), [])
+
+  let height = 0
+  const transitions = useTransition(
+    rows.map(data => ({ ...data, y: (height += data.height) - data.height })),
+    d => d.name,
+    {
+      from: { height: 0, opacity: 0 },
+      leave: { height: 0, opacity: 0 },
+      enter: ({ y, height }) => ({ y, height, opacity: 1 }),
+      update: ({ y, height }) => ({ y, height })
+    }
+  )
+
+  return (
+    <div className="list" style={{ height }}>
+      {transitions.map(({ item, props: { y, ...rest }, key }, index) => (
+        <animated.div
+          key={key}
+          className="card"
+          style={{ zIndex: data.length - index, transform: y.interpolate(y => `translate3d(0,${y}px,0)`), ...rest }}>
+          <div className="cell">
+            <div className="details" style={{ backgroundImage: item.css }} />
+          </div>
+        </animated.div>
+      ))}
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
